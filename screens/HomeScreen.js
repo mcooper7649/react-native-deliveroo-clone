d1c96ee60652dd233a40ff8c8ef849aade89c9a1
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-08-17 11:03:43 -0400

    add featuredrow, restaraunt card to homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,11 @@
-import { SafeAreaView, Text, Image, View, TextInput } from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  View,
+  TextInput,
+  ScrollView,
+} from 'react-native';
 import React, { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import {
@@ -7,6 +14,8 @@ import {
   SearchIcon,
   AdjustmentsIcon,
 } from 'react-native-heroicons/outline';
+import Categories from '../components/Categories';
+import FeaturedRow from '../components/FeaturedRow';
 
 export const HomeScreen = () => {
   const navigation = useNavigation();
@@ -18,36 +27,66 @@ export const HomeScreen = () => {
   }, []);
   return (
     <SafeAreaView className="bg-white pt-5">
-      <Text className="text-red-500">
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
-          <Image
-            source={{
-              uri: 'https://links.papareact.com/wru',
-            }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
+        <Image
+          source={{
+            uri: 'https://links.papareact.com/wru',
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+        <UserIcon size={35} color="#00CCBB" />
+
+        {/* Search */}
+      </View>
+      <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <SearchIcon color="gray" size={20} />
+          <TextInput
+            placeholder="restaraunts and cuisines"
+            keyboardType="default"
           />
-          <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">
-              Deliver Now!
-            </Text>
-            <Text className="font-bold text-xl">
-              Current Location
-              <ChevronDownIcon size={20} color="#00CCBB" />
-            </Text>
-          </View>
-          <UserIcon size={35} color="#00CCBB" />
         </View>
-        <View>
-          <View className="flex-row space-x-2 bg-gray-200">
-            <SearchIcon />
-            <TextInput
-              placeholder="restaraunts and cuisines"
-              keyboardType="default"
-            />
-          </View>
-          <AdjustmentsIcon color="#00CCBB" />
-        </View>
-      </Text>
+        <AdjustmentsIcon color="#00CCBB" />
+      </View>
+
+      {/* Body */}
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        {/* Categories */}
+        <Categories />
+        {/* Featurured*/}
+
+        <FeaturedRow
+          id="testing123"
+          title="Featured"
+          description="Paid placements form our partners"
+          featuredCategory="featured"
+        />
+        {/* Tasty Discount */}
+        <FeaturedRow
+          id="testing1234"
+          title="Featured"
+          description="Paid placements form our partners"
+          featuredCategory="featured"
+        />
+        {/* Offers Near You */}
+        <FeaturedRow
+          id="testing1235"
+          title="Featured"
+          description="Paid placements form our partners"
+          featuredCategory="featured"
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
